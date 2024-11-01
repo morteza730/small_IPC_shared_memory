d5210694ca1839db1c6a0a3dc427be5649b93ca4
morteza.sadeghi <student13730@gmail.com>
2024-11-01 09:30:24 +0330

    third stable version

--- src/m1.cpp
+++ src/m1.cpp
@@ -2,7 +2,6 @@
 #include <csignal>
 #include "SharedMemory.h"
 #include "notifier.h"
-#include "subprocess.h"
 #include "Buffer.h"
 #include <iostream>
 #include <unistd.h>
@@ -12,71 +11,34 @@ using namespace std;
 
 int main()
 {
+    int id = 1;
+    
     Notifer buffLck;
     buffLck.init(0xfa00);
 
     Buffer buffer;
     buffer.init(0x3333);
 
-    uint8_t value;
+    uint8_t value[6];
 
     std::cout << "please enter a value>> " << endl;
     do
     {
         cin >> value;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         if (cin)
         {
-            std::cout<<"sent value is: "<<value<<std::endl;
-            printf("sent value is: %hhu\n", value);
-            buffer.write(value);
+            std::cout << id << ":sent value is: " << value[0] << std::endl;
+            buffer.write(value[0]);
             buffLck.notify();
+        }
+        else
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
         if (buffLck.wait())
         {
-            printf("received value is: %hhu\n", buffer.read());
+            std::cout << id << ": received value is: " << buffer.read() << std::endl;
         }
     } while (cin);
 }
-
-// int main()
-// {
-//     uint8_t id_num[3] = {0,0,1};
-//     uint8_t mti_num[4] = {1,2,3,4};
-//     uint8_t pan_num[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-//     uint8_t destination_num[3] = {0,0,2};
-
-//     Notifer buffLck;
-//     buffLck.init(0xfa00);
-
-//     Message* msg = new Message{};
-//     SharedMemory sMemory;
-//     if (sMemory.init((void **)&msg, 0x1234, 40))
-//     {
-//         if (msg == nullptr)
-//             return -1;
-//         printf("You are the first one! Please init the memory if needed!\n");
-//     }
-
-//     // SubProcess sProcess{id_num,mti_num,pan_num};
-
-//     std::cout << "please enter the trace value>> " << endl;
-//     uint8_t trace[6];
-//     do
-//     {
-//         cin >> trace;
-//         cin.clear(); // Clear the error state
-//         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-//         if (cin)
-//         {
-//             for (int i=0;i<6;i++)
-//                 msg->Trace[i] = trace[i];
-//             buffLck.notify();
-//         }
-//         if (buffLck.wait())
-//         {
-//             printf("a = %i\n", msg->id[2]);
-//         }
-//     } while (cin);
-// }
--- src/m2.cpp
+++ src/m2.cpp
@@ -3,63 +3,32 @@
 #include <csignal>
 #include <unistd.h>
 #include "SharedMemory.h"
-#include "subprocess.h"
 #include "notifier.h"
 #include "Buffer.h"
 
 using namespace std;
 
 atomic<bool> terminate_flag(false);
-// void signalHandler(int signum) {terminate_flag = true;}
 
 int main()
 {
+    int id = 2;
+
     Notifer buffLck;
     buffLck.init(0xfa00);
 
     Buffer buffer;
     buffer.init(0x3333);
 
-    uint8_t value;
-
     while (!terminate_flag)
     {
         if (buffLck.wait())
         {
-            printf("a = %hhu \n", buffer.read());
-            buffer.write(35);
+            char valueBack = buffer.read();
+            std::cout << id << ": received value is: " << valueBack << std::endl;
+            buffer.write('b');
+            std::cout << id << ": sent value is: " << 'b' << std::endl;
             buffLck.notify();
         }
     }
 }
-
-// int main()
-// {
-//     uint8_t id_num[3] = {0,0,2};
-//     uint8_t mti_num[4] = {1,2,3,4};
-//     uint8_t pan_num[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-//     uint8_t destination_num[3] = {0,0,1};
-
-//     Notifer buffLck;
-//     buffLck.init(0xfa00);
-
-//     Message* msg = new Message{};
-//     SharedMemory sMemory;
-//     if (sMemory.init((void **)&msg, 0x1234, 40))
-//     {
-//         if (msg == nullptr)
-//             return -1;
-//         printf("You are the first one! Please init the memory if needed!\n");
-//     }
-
-//     while (!terminate_flag)
-//     {
-//         if (buffLck.wait())
-//         {
-//             printf("a = %i\n", msg->id[2]);
-//             msg->id[2] = 2;
-//         }
-//         // sProcess.sendback();
-//         buffLck.notify();
-//     }
-// }
